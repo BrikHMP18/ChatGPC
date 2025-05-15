4b78747cf115b1a0a3da041e89b8e4c27e2701a3
BrikHMP18 <your-email@example.com>
2025-05-15 10:31:46 -0500

    Postgress for the vector Database

--- server/embeddings.js
+++ server/embeddings.js
@@ -3,12 +3,24 @@ import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { Document } from '@langchain/core/documents';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
-// Embedding model
+import { PGVectorStore } from "@langchain/community/vectorstores/pgvector";
+
 const embeddings = new OpenAIEmbeddings({
   model: 'text-embedding-3-large',
 });
-
-export const vectorStore = new MemoryVectorStore(embeddings);
+export const vectorStore = await PGVectorStore.initialize(embeddings, {
+    postgresConnectionOptions: {
+        connectionString: process.env.DB_URL,
+    },
+    tableName: 'documents',
+    columns: {
+        idColumnName: 'id',
+        vectorColumnName: 'vector',
+        contentColumnName: 'content',
+        metadataColumnName: 'metadata',
+    },
+    distanceStrategy: 'cosine',
+})
 
 export const addDocumentToVectorStore = async (docArray) => {
   const docs = docArray.map(doc =>
